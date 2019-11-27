e47ffced710443171c6f5e46f70fe034e9e21571
Gitea <tony.fortesramos@ogd.nl>
2019-11-27 22:58:12 +0100

    Fix shizzle

---
+++ start.sh
@@ -0,0 +1,29 @@
+if ! git diff --no-ext-diff --quiet --exit-code; then
+    SSH_PATH="./.ssh"
+    KEY_FILENAME="id_rsa"
+    mkdir -p "${SSH_PATH}"
+    chmod 700 "${SSH_PATH}"
+
+    echo ${GHA_DEPLOY_KEY} > "${SSH_PATH}/${KEY_FILENAME}"
+    cat "${SSH_PATH}/${KEY_FILENAME}"
+    chmod 600 "${SSH_PATH}/${KEY_FILENAME}"
+
+    whoami
+    eval `ssh-agent -t 60 -s`
+    ssh-add ${SSH_PATH}/${KEY_FILENAME}
+
+    cd public
+    pwd
+
+    git config --local user.name "${GITHUB_ACTOR}"
+    git config --local user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+    git config user.name
+    git config user.email
+    git add .
+    echo -n 'Files to Commit:' && ls -l | wc -l
+    timestamp=$(date +%s%3N)
+    git commit -a -m "Automated deployment to GitHub Pages on $timestamp"
+    # echo
+
+    echo "Done!"
+fi
