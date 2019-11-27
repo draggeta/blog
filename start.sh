9b750dd8c00e8391452950b9becb8f246f90d732
Gitea <tony.fortesramos@ogd.nl>
2019-11-27 23:18:44 +0100

    Test :D

--- start.sh
+++ start.sh
@@ -1,30 +1,38 @@
 #! /bin/bash
 if ! git diff --no-ext-diff --quiet --exit-code; then
     SSH_PATH="~/.ssh"
-    KEY_FILENAME="id_rsa2"
+    KEY_FILENAME="id_rsa"
     mkdir -p "${SSH_PATH}"
     chmod 700 "${SSH_PATH}"
 
     echo ${GHA_DEPLOY_KEY} > "${SSH_PATH}/${KEY_FILENAME}"
-    cat "${SSH_PATH}/${KEY_FILENAME}"
     chmod 600 "${SSH_PATH}/${KEY_FILENAME}"
 
-    whoami
+    ssh-keyscan github.com >> "${SSH_PATH}/known_hosts"
+    chmod 644 "${SSH_PATH}/known_hosts"
+
     eval `ssh-agent -t 60 -s`
     ssh-add ${SSH_PATH}/${KEY_FILENAME}
 
     cd public
-    pwd
 
     git config --local user.name "${GITHUB_ACTOR}"
     git config --local user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-    git config user.name
-    git config user.email
+
     git add .
+
     echo -n 'Files to Commit:' && ls -l | wc -l
     timestamp=$(date +%s%3N)
-    git commit -a -m "Automated deployment to GitHub Pages on $timestamp"
-    # echo
+    git commit -am "Automated deployment to GitHub Pages on $timestamp"
+
+    git push origin master
+
+    # #       git remote set-url origin "$(git config --get remote.origin.url | sed 's#http.*com/#git@github.com:#g')"
+    # #       mkdir -p ~/.ssh/
+    # #       ssh-keyscan github.com >> ~/.ssh/known_hosts
+    # #       git push
+    # #       ssh-agent -k
+
 
     echo "Done!"
 fi
