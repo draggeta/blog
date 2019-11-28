94074cc5c0492d8b46638b1ca800a56542574733
Gitea <tony.fortesramos@ogd.nl>
2019-11-28 23:57:58 +0100

    Final test run

--- start.sh
+++ start.sh
@@ -10,13 +10,13 @@ apk add --update git openssh-client bash git-subtree ca-certificates
 if ! git diff --no-ext-diff --quiet --exit-code; then
     # enable ssh-agent, as it's not running in the container and add the key
     eval `ssh-agent`
-    printf "%s" "${GHA_DEPLOY_KEY}" | ssh-add - #
+    printf "%s" "${GHA_DEPLOY_KEY}" | ssh-add -
 
     cd public
 
     git config --local user.name "${GITHUB_ACTOR}"
     git config --local user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-    git config --global core.sshCommand "ssh -o IdentitiesOnly=no -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -F /dev/null" #"
+    git config --global core.sshCommand "ssh -o IdentitiesOnly=no -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -F /dev/null"
     git config --global status.submodulesummary 1
     git config --global diff.submodule log
 
