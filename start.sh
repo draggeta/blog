7406799d2e3bff4af924de95b2d87d0663589f92
Gitea <tony.fortesramos@ogd.nl>
2019-11-28 14:46:23 +0100

    Test

--- start.sh
+++ start.sh
@@ -30,7 +30,7 @@ if ! git diff --no-ext-diff --quiet --exit-code; then
 
     git config --local user.name "${GITHUB_ACTOR}"
     git config --local user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-    git config --global core.sshCommand "ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${SSH_PATH}/id_rsa -F /dev/null"
+    git config --global core.sshCommand "ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${SSH_PATH}/${KEY_FILENAME} -F /dev/null"
     git config --global status.submodulesummary 1
     git config --global diff.submodule log
 
@@ -44,6 +44,8 @@ if ! git diff --no-ext-diff --quiet --exit-code; then
     git commit -am "Automated deployment to GitHub Pages on $timestamp"
 
     git remote set-url origin "$(git config --get remote.origin.url | sed 's#http.*com/#git@github.com:#g')"
+
+    git diff origin/master
 
     git push origin master
 
