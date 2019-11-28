8accb02a274ba0c87086d526ebae0ac43b04525c
Gitea <tony.fortesramos@ogd.nl>
2019-11-28 23:46:55 +0100

    Test

--- start.sh
+++ start.sh
@@ -15,24 +15,27 @@ if ! git diff --no-ext-diff --quiet --exit-code; then
     mkdir -p "${SSH_PATH}"
     chmod 750 "${SSH_PATH}"
 
-    printf "%s" "${GHA_DEPLOY_KEY}" > "${SSH_PATH}/${KEY_FILENAME}"
-    chmod 600 "${SSH_PATH}/${KEY_FILENAME}"
+    # printf "%s" "${GHA_DEPLOY_KEY}" > "${SSH_PATH}/${KEY_FILENAME}"
+    # chmod 600 "${SSH_PATH}/${KEY_FILENAME}"
 
-    # add github.com to the known hosts file
-    ssh-keyscan github.com >> "${SSH_PATH}/known_hosts"
-    chmod 640 "${SSH_PATH}/known_hosts"
+    # # add github.com to the known hosts file
+    # ssh-keyscan github.com >> "${SSH_PATH}/known_hosts"
+    # chmod 640 "${SSH_PATH}/known_hosts"
 
-    chown root:root -R "${SSH_PATH}"
+    # chown root:root -R "${SSH_PATH}"
 
     # enable ssh-agent, as it's not running in the container and add the key
     eval `ssh-agent`
-    ssh-add ${SSH_PATH}/${KEY_FILENAME}
+    printf "%s" "${GHA_DEPLOY_KEY}" > ssh-add - #
+    ssh-add -l #
+    # ssh-add ${SSH_PATH}/${KEY_FILENAME}
 
     cd public
 
     git config --local user.name "${GITHUB_ACTOR}"
     git config --local user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-    git config --global core.sshCommand "ssh -i ${SSH_PATH}/${KEY_FILENAME} -o IdentitiesOnly=yes -o UserKnownHostsFile=${SSH_PATH}/known_hosts -F /dev/null" #-o StrictHostKeyChecking=no"
+    # git config --global core.sshCommand "ssh -i ${SSH_PATH}/${KEY_FILENAME} -o IdentitiesOnly=yes -o UserKnownHostsFile=${SSH_PATH}/known_hosts -F /dev/null" #-o StrictHostKeyChecking=no"
+    git config --global core.sshCommand "ssh -o IdentitiesOnly=no -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -F /dev/null" #"
     git config --global status.submodulesummary 1
     git config --global diff.submodule log
 
