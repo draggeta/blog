3b93193d488f60bd3db662fb4009f74968a8af4e
Gitea <tony.fortesramos@ogd.nl>
2019-11-28 22:37:47 +0100

    Yay!

--- start.sh
+++ start.sh
@@ -4,33 +4,36 @@ apk add --update git openssh-client bash git-subtree         \
     ca-certificates
 
 if ! git diff --no-ext-diff --quiet --exit-code; then
-    SSH_PATH="${GITHUB_WORKSPACE}/.ssh"
+    SSH_PATH="~/.ssh"
+    TEMP_SSH_PATH="${GITHUB_WORKSPACE}/.ssh"
     KEY_FILENAME="id_rsa"
-    mkdir -p "${SSH_PATH}"
-    chmod 750 "${SSH_PATH}"
+    mkdir -p "${TEMP_SSH_PATH}"
+    chmod 750 "${TEMP_SSH_PATH}"
 
-    # echo ${GHA_DEPLOY_KEY} > "${SSH_PATH}/${KEY_FILENAME}"
-    # cp "${GITHUB_WORKSPACE}/.ssh/${KEY_FILENAME}" "${SSH_PATH}/${KEY_FILENAME}"
-    chmod 600 "${SSH_PATH}/${KEY_FILENAME}"
+    # echo ${GHA_DEPLOY_KEY} > "${TEMP_SSH_PATH}/${KEY_FILENAME}"
+    # cp "${GITHUB_WORKSPACE}/.ssh/${KEY_FILENAME}" "${TEMP_SSH_PATH}/${KEY_FILENAME}"
+    chmod 600 "${TEMP_SSH_PATH}/${KEY_FILENAME}"
 
-    # echo -e "Host github.com\n\tIdentityFile ${SSH_PATH}/${KEY_FILENAME}\n\tStrictHostKeyChecking no\n\tAddKeysToAgent yes\n" >> "${SSH_PATH}/config"
-    # chmod 640 "${SSH_PATH}/config"
+    echo -e "Host github.com\n\tIdentityFile ${SSH_PATH}/${KEY_FILENAME}\n\tStrictHostKeyChecking no\n\tAddKeysToAgent yes\n" >> "${SSH_PATH}/config"
+    chmod 640 "${SSH_PATH}/config"
 
-    # ssh-keyscan github.com >> "${SSH_PATH}/known_hosts"
-    # chmod 640 "${SSH_PATH}/known_hosts"
+    ssh-keyscan github.com >> "${SSH_PATH}/known_hosts"
+    chmod 640 "${SSH_PATH}/known_hosts"
 
     chown root:root -R "${SSH_PATH}"
+    chown root:root -R "${TEMP_SSH_PATH}"
 
     ls -al "${SSH_PATH}/"
+    ls -al "${TEMP_SSH_PATH}/"
 
     eval `ssh-agent`
-    ssh-add ${SSH_PATH}/${KEY_FILENAME}
+    ssh-add ${TEMP_SSH_PATH}/${KEY_FILENAME}
 
     cd public
 
     git config --local user.name "${GITHUB_ACTOR}"
     git config --local user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-    git config --global core.sshCommand "ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${SSH_PATH}/${KEY_FILENAME} -F /dev/null"
+    # git config --global core.sshCommand "ssh -o IdentitiesOnly=yes" # -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${TEMP_SSH_PATH}/${KEY_FILENAME} -F /dev/null"
     git config --global status.submodulesummary 1
     git config --global diff.submodule log
 
@@ -51,12 +54,7 @@ if ! git diff --no-ext-diff --quiet --exit-code; then
 
     git merge temp_data
 
-    git push # origin master
-
-
-    # #       git push
-    # #       ssh-agent -k
-
+    git push
 
     echo "Done!"
 fi
