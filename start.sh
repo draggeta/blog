381f68d655e1c649f1f1a14c4a5f5fa27b4f3700
Gitea <tony.fortesramos@ogd.nl>
2019-11-28 23:01:35 +0100

    Wooo!

--- start.sh
+++ start.sh
@@ -10,12 +10,10 @@ if ! git diff --no-ext-diff --quiet --exit-code; then
     mkdir -p "${SSH_PATH}"
     chmod 750 "${SSH_PATH}"
 
-    # echo ${GHA_DEPLOY_KEY} > "${SSH_PATH}/${KEY_FILENAME}"
+    echo ${GHA_DEPLOY_KEY} > "${SSH_PATH}/${KEY_FILENAME}2"
+    diff "${SSH_PATH}/${KEY_FILENAME}" "${SSH_PATH}/${KEY_FILENAME}2"
     # cp "${GITHUB_WORKSPACE}/.ssh/${KEY_FILENAME}" "${SSH_PATH}/${KEY_FILENAME}"
     chmod 600 "${SSH_PATH}/${KEY_FILENAME}"
-
-    # echo -e "Host github.com\n\tIdentityFile ${SSH_PATH}/${KEY_FILENAME}\n\tStrictHostKeyChecking no\n\tAddKeysToAgent yes\n" >> "${SSH_PATH}/config"
-    # chmod 640 "${SSH_PATH}/config"
 
     ssh-keyscan github.com >> "${SSH_PATH}/known_hosts"
     chmod 640 "${SSH_PATH}/known_hosts"
