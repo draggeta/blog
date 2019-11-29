3831ad1b55d7b1d3dd7d8d59dfb4419c50678a91
Gitea <tony.fortesramos@ogd.nl>
2019-11-29 01:19:47 +0100

    Woops

--- .github/scripts/add.sh
+++ .github/scripts/add.sh
@@ -6,6 +6,15 @@ set -e
 # add required packages for git to run
 apk add --update git openssh-client bash git-subtree ca-certificates
 
+eval `ssh-agent`
+printf "%s" "${GHA_DEPLOY_KEY}" | ssh-add -
+
+git config --local user.name "${GITHUB_ACTOR}"
+git config --local user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git config --global core.sshCommand "ssh -o IdentitiesOnly=no -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -F /dev/null"
+git config --global status.submodulesummary 1
+git config --global diff.submodule log
+
 git submodule add -b master git@github.com:draggeta/draggeta.github.io.git public
 
 echo "Done!"
