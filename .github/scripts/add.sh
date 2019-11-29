cac2b83e8a4627e26baa8b09cb124fcd213b9086
Gitea <tony.fortesramos@ogd.nl>
2019-11-29 01:18:13 +0100

    Fix mistake

--- .github/scripts/add.sh
+++ .github/scripts/add.sh
@@ -5,7 +5,6 @@ set -e
 
 # add required packages for git to run
 apk add --update git openssh-client bash git-subtree ca-certificates
-+
 
 git submodule add -b master git@github.com:draggeta/draggeta.github.io.git public
 
