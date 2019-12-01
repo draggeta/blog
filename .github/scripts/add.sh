b85ecc730493ac4863acf0f5c8020fa8020d5c14
Gitea <tony.fortesramos@ogd.nl>
2019-12-01 23:00:58 +0100

    Test

--- .github/scripts/add.sh
+++ .github/scripts/add.sh
@@ -28,21 +28,45 @@ git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 printf "Add the pages repository as a submodule\n"
 git submodule add -b master git@github.com:draggeta/draggeta.github.io.git public
 
-printf "Set the current workdir to ${GITHUB_WORKSPACE}/public"
+printf "Set the current workdir to ${GITHUB_WORKSPACE}/public\n"
 cd ${GITHUB_WORKSPACE}/public
 
 # remove all files and folders except .git folder
 printf "Clean the pages repository\n"
 find . -mindepth 1 -maxdepth 1 ! -regex '^\./\.git' -exec rm -rf {} \;
 
-printf "Set the current workdir to ${GITHUB_WORKSPACE}"
+printf "Set the current workdir to ${GITHUB_WORKSPACE}\n"
 cd ${GITHUB_WORKSPACE}
 
-printf "Generate the pages with Hugo"
+printf "Generate the pages with Hugo\n"
 hugo
 
-printf "Set the current workdir to ${GITHUB_WORKSPACE}/public"
+printf "Set the current workdir to ${GITHUB_WORKSPACE}/public\n"
 cd ${GITHUB_WORKSPACE}/public
-ls -al
+
+printf "Check for changes in git\n"
+
+# if differences are found, run
+if ! git diff --no-ext-diff --quiet --exit-code; then
+    # enable ssh-agent, as it's not running in the container and add the key
+    printf "Enable and set up SSH agent\n"
+    eval `ssh-agent`
+    printf "%s" "${GHA_DEPLOY_KEY}" | ssh-add -
+
+    printf "Stage all files in the pages repository\n"
+    cd public
+    git add .
+
+    printf "Commit all files in the pages repository\n"
+    echo -n 'Files to Commit:' && ls -l | wc -l
+    git commit -am "Automated deployment to GitHub Pages on $(date)"
+
+    printf "Push all changes to the pages repository\n"
+    git push
+
+else
+    printf "No changes found\n"
+
+fi
 
 printf "Done!"
